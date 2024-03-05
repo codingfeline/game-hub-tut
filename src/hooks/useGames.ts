06ff69ec8d9bfe13983f5297384e63db4b78145a
Nazie Sharid <nazie.sh4rid@gmail.com>
2024-03-05 18:46:19 +0000

    moved interfaces out of component

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,19 +2,20 @@ import { useEffect, useState } from "react"
 import apiClient from "../services/api-client"
 import { CanceledError } from "axios"
 
+interface Game {
+  id: number
+  name: string
+}
+
+interface FetchGamesResponse {
+  count: number
+  results: Game[]
+}
+
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([])
   const [error, setError] = useState('')
 
-  interface Game {
-    id: number
-    name: string
-  }
-
-  interface FetchGamesResponse {
-    count: number
-    results: Game[]
-  }
 
   useEffect(() => {
     const controller = new AbortController()
@@ -29,7 +30,7 @@ const useGames = () => {
         setError(err.message)
       })
 
-    return () => controller.abort()
+    return () => controller.abort() // clean-up function
   }, [])
 
   return { games, error }
