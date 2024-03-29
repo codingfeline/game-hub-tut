870344332ea6c5b619fe5eb68ced53c458470bc3
Nazie Sharid <nazie.sh4rid@gmail.com>
2024-03-29 15:33:45 +0000

    add routes

---
+++ src/pages/Layout.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavBar from '../components/NavBar'
+import { Outlet } from 'react-router-dom'
+
+const Layout = () => (
+  <div>
+    <NavBar />
+    <Outlet />
+  </div>
+)
+
+export default Layout
